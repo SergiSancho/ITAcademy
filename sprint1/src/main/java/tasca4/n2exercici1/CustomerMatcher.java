460cabaa73f14128a5e11efbac7bb172663d036b
SeRGe <sergiosamo@protonmail.com>
2024-06-25 10:54:30 +0200

    revisado matxer

--- sprint1/src/main/java/tasca4/n2exercici1/CustomerMatcher.java
+++ sprint1/src/main/java/tasca4/n2exercici1/CustomerMatcher.java
@@ -1,17 +1,7 @@
 package tasca4.n2exercici1;
 
-import org.hamcrest.FeatureMatcher;
-import org.hamcrest.Matcher;
-
-import static org.hamcrest.CoreMatchers.is;
-
 public class CustomerMatcher {
-    public static Matcher<String> hasLength(int length) {
-        return new FeatureMatcher<String, Integer>(is(length), "una cadena amb longitud", "longitud") {
-            @Override
-            protected Integer featureValueOf(String actual) {
-                return actual.length();
-            }
-        };
+    public static void main(String[] args) {
+
     }
 }
--- sprint1/src/test/java/tasca4/n2exercici1/CustomerMatcherTest.java
+++ sprint1/src/test/java/tasca4/n2exercici1/CustomerMatcherTest.java
@@ -1,15 +1,27 @@
 package tasca4.n2exercici1;
 
-
 import org.junit.jupiter.api.Test;
-
-import static tasca4.n2exercici1.CustomerMatcher.hasLength;
 import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.Matcher;
+import org.hamcrest.FeatureMatcher;
+import static org.hamcrest.Matchers.*;
 
-public class CustomerMatcherTest {
+class ExerciciTest {
+
     @Test
-    public void testStringLengthMatcher() {
-        String testString = "Mordor";
-        assertThat(testString, hasLength(8));
+    void testLength() {
+        String str = "Mordor";
+        assertThat(str, length(equalTo(8)));
+    }
+
+    Matcher<String> length(Matcher<? super Integer> matcher) {
+        return new FeatureMatcher<String, Integer>(matcher,
+                "una cadena amb longitut", "longitut") {
+
+            @Override
+            protected Integer featureValueOf(String str) {
+                return str.length();
+            }
+        };
     }
 }
