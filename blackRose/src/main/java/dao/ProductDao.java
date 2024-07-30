4cc2ef428d2868971fc4d0c2adb20780cac7743e
SeRGe <sergiosamo@protonmail.com>
2024-07-30 20:20:32 +0200

    nada

--- blackRose/src/main/java/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-//ELIMINAR DAO PRODUCT, ESTOS METODOS ESTADISTICA EN PAQUETE SERVICE
-import model.Product;
-
-public interface ProductDao<ID> extends GenericDao<Product<ID>, ID> {
-   // int getTotalStockForProducts();
-
-    // double getTotalValueForProducts();
-}
--- blackRose/src/main/java/dao/TicketDetailDao.java
+++
@@ -1,6 +0,0 @@
-package dao;
-
-import model.TicketDetail;
-
-public interface TicketDetailDao<ID> extends GenericDao<TicketDetail<ID>, ID> {
-}
